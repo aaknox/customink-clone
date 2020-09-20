f645241fcc545af8889aa123cf300f2178aaae6d
Azhya Knox <56852910+aaknox@users.noreply.github.com>
2020-09-20 11:59:55 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-//********************HEADER********************
-//fade effect on header bar
-$(window).scroll(function() {
-    if($(this).scrollTop() > 10) {
-        $('.image-box').addClass('opaque');
-        $('.info-box').addClass('opaque');
-        $('.image-box').removeClass('viewable');
-        $('.info-box').removeClass('viewable');
-    } else {
-        $('.image-box').addClass('viewable');
-        $('.info-box').addClass('viewable');
-        $('.image-box').removeClass('opaque');
-        $('.info-box').removeClass('opaque');
-    }
-});
-//*************************************************
-
 //****************AUTO IMAGE CHANGE*****************
 //change image in ad-image box after 5 seconds
 var images = [
