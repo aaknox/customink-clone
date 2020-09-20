9aecd4187b03b5be4f018c4f0cc883169618a6a8
Azhya Knox <56852910+aaknox@users.noreply.github.com>
2020-09-20 11:39:40 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -134,19 +134,3 @@ $(document).ready(function(){
 /**********END LOOP *********/
 
 //**********************************
-
-
-/**********GOOGLE MAP*************/
-function initMap(){
-  var location = {
-    lat: -25.363,
-    lng: 131.044
-  };
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: location
-  });
-  //get key from google map api site
-  
-}
-/********END GOOGLE MAP***********/
