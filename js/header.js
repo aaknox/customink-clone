63c03ad39a44463c9bb9b8c8680d52fe43bf9ce9
Azhya Knox <56852910+aaknox@users.noreply.github.com>
2020-09-20 12:00:12 -0500

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,16 @@
-
+//********************HEADER********************
+//fade effect on header bar
+$(window).scroll(function() {
+    if($(this).scrollTop() > 10) {
+        $('.image-box').addClass('opaque');
+        $('.info-box').addClass('opaque');
+        $('.image-box').removeClass('viewable');
+        $('.info-box').removeClass('viewable');
+    } else {
+        $('.image-box').addClass('viewable');
+        $('.info-box').addClass('viewable');
+        $('.image-box').removeClass('opaque');
+        $('.info-box').removeClass('opaque');
+    }
+});
+//*************************************************
